f79f135bc8e9d6506f3178b968506c819721b2f9
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2020-06-17 19:56:54 +0530

    Added product ids to cart items

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,15 +1,7 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            price: 999,
-            title: 'Mobile Phone',
-            qty: 1,
-            img: ''
-        }
-    }
+    
     increaseQuantity = () => {
         // console.log('this', this.state);
         // this.setState({
@@ -33,9 +25,11 @@ class CartItem extends React.Component {
         });
     }
     render() {
-        const {price, title, qty} = this.state
+        console.log('this.props', this.props);
+        const {price, title, qty} = this.props.product;
         return (
             <div className="cart-item">
+                {this.props.jsx}
                 <div className="left-block">
                     <img style={styles.image} />
                 </div>
